221ce596fb214312ebc2e0f4cb33eb562ef0555a
Deirdre Cavins <dcavins53@gmail.com>
2026-02-18 16:43:54 -0600

    event handling

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const form =document.getElementById("json-form")
 const input =document.getElementById("form-input")
 const output =document.querySelector(".output")
-
+form.addEventListener("submit", (event)=>{
+    event.preventDefault()
+})
