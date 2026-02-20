ab4e6b0b15ea33c716845d27559adc212a3e3fd6
Deirdre Cavins <dcavins53@gmail.com>
2026-02-20 15:29:10 -0600

    completed week3 day 3

--- app.js
+++ app.js
@@ -2,8 +2,24 @@
 console.log("Hello")
 const form =document.getElementById("jsonForm")
 const input =document.getElementById("jsonInput")
-const output =document.querySelector(".output")
+const output =document.querySelector("#output")
+const output2 =document.querySelector("#output2")
+const output3 =document.querySelector("#output3")
+
 form.addEventListener("submit",(event)=>{
     event.preventDefault();
+    try{
+        let items =JSON.parse(input.value)
+        console.log(items)
+        output.textContent = items[0]
+        output2.textContent = items[1]
+        output3.textContent = items[2]
+
+    }
+catch(error){
+    console.error("error parsing JSON", error)
+}
+
+
 });
 
